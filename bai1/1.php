62d7ebb1938f02f4b37d01b4eab93b2e833da68c
MANH NGUYEN <you@example.com>
2018-10-12 04:39:48 +0700

    nguyen

---
+++ bai1/1.php
@@ -0,0 +1,21 @@
+<?php echo $var ?>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class ModelName extends CI_Model {
+
+	public $variable;
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+	public function FunctionName($value='')
+	{
+		# code...
+	}
+
+}
+
+/* End of file 1.php */
+/* Location: ./application/models/1.php */
